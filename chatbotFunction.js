78a1bf6e6b7cb09f8c81e5b2a0cbfe77cc2a7651
arya321123 <74927818+arya321123@users.noreply.github.com>
2023-12-17 20:35:41 +0700

    upgrade
    hola

---
+++ chatbotFunction.js
@@ -0,0 +1,19 @@
+/* eslint-disable max-len */
+export const tokenizeInput = (input) => {
+  return input.toLowerCase().split(/[ ,.?!]+/);
+};
+
+export const matchInputToIntent = (input, intents) => {
+  const tokens = tokenizeInput(input);
+
+  for (const intent of intents) {
+    for (const keyword of intent.input) {
+      if (tokens.includes(keyword.toLowerCase())) {
+        return intent.responses[Math.floor(Math.random() * intent.responses.length)];
+      }
+    }
+  }
+
+  return 'Maaf, saya tidak mengerti pertanyaan Anda.';
+};
+
